fa5b3e72d300e051b72b1a60cc14ab13829287ff
Herman-Gathege <siinamtechstudio@gmail.com>
2024-09-01 13:37:51 +0300

    added now

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # If the length is 0, print an empty list
+    if length == 0:
+        print([])
+        return
+
+    # Start with the first two numbers of the Fibonacci sequence
+    fib_sequence = [0, 1]
+
+    # If the requested length is 1, only include the first number
+    if length == 1:
+        print([0])
+        return
+
+    # Generate the Fibonacci sequence until it reaches the required length
+    while len(fib_sequence) < length:
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    # Print the sequence up to the required length
+    print(fib_sequence[:length])
